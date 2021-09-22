746fa33a0dcaef556dc6848ea2d47b6ffd32e124
심향택(H) <htsim@hbedu.co.kr>
2021-09-22 23:59:15 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleat/ThymeleatBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleatBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleatBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/thymeleat/ThymeleatBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymeleat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleatBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
